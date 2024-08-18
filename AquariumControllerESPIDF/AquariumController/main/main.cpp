2bb96db3e08da0fd204e50b6f5ae8a45f91a6647
Connor Williams <c.jwilliams@me.com>
2024-08-18 08:56:44 -0600

    Still Has Issues
    Still has issues emulating hardware components.

--- AquariumControllerESPIDF/AquariumController/main/main.cpp
+++ AquariumControllerESPIDF/AquariumController/main/main.cpp
@@ -4,26 +4,8 @@
 #include "AqWebServer.h"
 #include <WiFi.h>
 #include "MailClient.h"
-#include "esp_phy_init.h"
+#include "Environment.h"
 
-extern "C" void app_main()
-{
-    initArduino();
-    //pinMode(4, OUTPUT);
-    //digitalWrite(4, HIGH);
-    ESP_ERROR_CHECK(nvs_flash_init());
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    //esp_phy_erase_cal_data_in_nvs();
-    setup();
-    EmailMessage eMessage;
-    eMessage.subject = "AQ Controller Notification";
-    eMessage.body = "AQ Controller Starting Up.";
-    xTaskCreate(sendEmailTask, "smtp_client_task", TASK_STACK_SIZE, (void*) &eMessage, tskIDLE_PRIORITY, NULL);
-    while(true) {
-      loop();
-    }
-}
 //-----------------------------Function Declarations-----------------------------
 void printLocalTime();
 void IRAM_ATTR taskTimerInterrupt();
@@ -58,7 +40,125 @@ const int   daylightOffset_sec = 3600;
 IPAddress IP = {192, 168, 0, 2};
 IPAddress gateway = {192, 168, 0, 2};
 IPAddress NMask = {255, 255, 255, 0};
+#ifdef ENVIRONMENT_DEV
+  #include "ethernet_init.c"
 
+  volatile SemaphoreHandle_t ethernetEventSemaphore;
+  /** Event handler for Ethernet events */
+  static void eth_event_handler(void *arg, esp_event_base_t event_base,
+                                int32_t event_id, void *event_data)
+  {
+      uint8_t mac_addr[6] = {0};
+      /* we can get the ethernet driver handle from event data */
+      esp_eth_handle_t eth_handle = *(esp_eth_handle_t *)event_data;
+
+      switch (event_id) {
+      case ETHERNET_EVENT_CONNECTED:
+          esp_eth_ioctl(eth_handle, ETH_CMD_G_MAC_ADDR, mac_addr);
+          ESP_LOGI(TAG, "Ethernet Link Up");
+          ESP_LOGI(TAG, "Ethernet HW Addr %02x:%02x:%02x:%02x:%02x:%02x",
+                  mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+          break;
+      case ETHERNET_EVENT_DISCONNECTED:
+          ESP_LOGI(TAG, "Ethernet Link Down");
+          break;
+      case ETHERNET_EVENT_START:
+          ESP_LOGI(TAG, "Ethernet Started");
+          break;
+      case ETHERNET_EVENT_STOP:
+          ESP_LOGI(TAG, "Ethernet Stopped");
+          break;
+      default:
+          break;
+      }
+  }
+
+  /** Event handler for IP_EVENT_ETH_GOT_IP */
+  static void got_ip_event_handler(void *arg, esp_event_base_t event_base,
+                                  int32_t event_id, void *event_data)
+  {
+      ip_event_got_ip_t *event = (ip_event_got_ip_t *) event_data;
+      const esp_netif_ip_info_t *ip_info = &event->ip_info;
+
+      ESP_LOGI(TAG, "Ethernet Got IP Address");
+      ESP_LOGI(TAG, "~~~~~~~~~~~");
+      ESP_LOGI(TAG, "ETHIP:" IPSTR, IP2STR(&ip_info->ip));
+      ESP_LOGI(TAG, "ETHMASK:" IPSTR, IP2STR(&ip_info->netmask));
+      ESP_LOGI(TAG, "ETHGW:" IPSTR, IP2STR(&ip_info->gw));
+      ESP_LOGI(TAG, "~~~~~~~~~~~");
+      configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+      if (getLocalTime(&timeinfo, 10000)) {
+      rtc.setTimeStruct(timeinfo);
+      } else {
+      rtc.setTime(0, 0, 0, 1, 1, 2024);
+      }
+  }
+#endif
+
+extern "C" void app_main()
+{
+    initArduino();
+    //pinMode(4, OUTPUT);
+    //digitalWrite(4, HIGH);
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    #ifdef ENVIRONMENT_DEV
+      //ethernetEventSemaphore = xSemaphoreCreateBinary();
+      // Initialize Ethernet driver
+      uint8_t eth_port_cnt = 0;
+      esp_eth_handle_t *eth_handles;
+      ESP_ERROR_CHECK(example_eth_init(&eth_handles, &eth_port_cnt));
+      // Create instance(s) of esp-netif for Ethernet(s)
+      if (eth_port_cnt == 1) {
+          // Use ESP_NETIF_DEFAULT_ETH when just one Ethernet interface is used and you don't need to modify
+          // default esp-netif configuration parameters.
+          esp_netif_config_t cfg = ESP_NETIF_DEFAULT_ETH();
+          esp_netif_t *eth_netif = esp_netif_new(&cfg);
+          // Attach Ethernet driver to TCP/IP stack
+          ESP_ERROR_CHECK(esp_netif_attach(eth_netif, esp_eth_new_netif_glue(eth_handles[0])));
+      } else {
+          // Use ESP_NETIF_INHERENT_DEFAULT_ETH when multiple Ethernet interfaces are used and so you need to modify
+          // esp-netif configuration parameters for each interface (name, priority, etc.).
+          esp_netif_inherent_config_t esp_netif_config = ESP_NETIF_INHERENT_DEFAULT_ETH();
+          esp_netif_config_t cfg_spi = {
+              .base = &esp_netif_config,
+              .stack = ESP_NETIF_NETSTACK_DEFAULT_ETH
+          };
+          char if_key_str[10];
+          char if_desc_str[10];
+          char num_str[3];
+          for (int i = 0; i < eth_port_cnt; i++) {
+              itoa(i, num_str, 10);
+              strcat(strcpy(if_key_str, "ETH_"), num_str);
+              strcat(strcpy(if_desc_str, "eth"), num_str);
+              esp_netif_config.if_key = if_key_str;
+              esp_netif_config.if_desc = if_desc_str;
+              esp_netif_config.route_prio -= i*5;
+              esp_netif_t *eth_netif = esp_netif_new(&cfg_spi);
+
+              // Attach Ethernet driver to TCP/IP stack
+              ESP_ERROR_CHECK(esp_netif_attach(eth_netif, esp_eth_new_netif_glue(eth_handles[i])));
+          }
+      }
+      // Register user defined event handers
+      ESP_ERROR_CHECK(esp_event_handler_register(ETH_EVENT, ESP_EVENT_ANY_ID, &eth_event_handler, NULL));
+      ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &got_ip_event_handler, NULL));
+
+      // Start Ethernet driver state machine
+      for (int i = 0; i < eth_port_cnt; i++) {
+          ESP_ERROR_CHECK(esp_eth_start(eth_handles[i]));
+      }
+    #endif
+    setup();
+    EmailMessage eMessage;
+    eMessage.subject = "AQ Controller Notification";
+    eMessage.body = "AQ Controller Starting Up.";
+    xTaskCreate(sendEmailTask, "smtp_client_task", TASK_STACK_SIZE, (void*) &eMessage, tskIDLE_PRIORITY, NULL);
+    while(true) {
+      loop();
+    }
+}
 
 void setup() {
   Serial.begin(115200);
@@ -71,35 +171,36 @@ void setup() {
       //SerialBT.begin("AqController");
   #endif
   
-
-  #ifdef useSerial
-      Serial.printf("Connecting to %s ", WIFI_SSID);
+  #ifdef ENVIRONMENT_PROD
+    #ifdef useSerial
+        Serial.printf("Connecting to %s ", WIFI_SSID);
+    #endif
+    #ifdef useSerialBT
+        SerialBT.printf("Connecting to %s ", WIFI_SSID);
+    #endif
+    WiFi.onEvent(WiFiStationHasIP, ARDUINO_EVENT_WIFI_STA_GOT_IP);
+    WiFi.onEvent(WiFiStationDisconnect, ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
+    WiFi.onEvent(WiFiScanComplete, ARDUINO_EVENT_WIFI_SCAN_DONE);
+    WiFi.onEvent(WiFiStaConnectedToSoftAP, ARDUINO_EVENT_WIFI_AP_STACONNECTED);
+    WiFi.disconnect(true);
+    WiFi.setHostname("AquariumController");
+    WiFi.mode(WIFI_STA);
+    WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, INADDR_NONE);
+    wifiEventSemaphore = xSemaphoreCreateBinary();
+    WiFi.setAutoReconnect(false);
+    WiFi.begin(ssid, password);
+    xSemaphoreTake(wifiEventSemaphore, portMAX_DELAY);
+    if (WiFi.status() == WL_CONNECTED) {
+        configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+        if (getLocalTime(&timeinfo, 10000)) {
+        rtc.setTimeStruct(timeinfo);
+        } else {
+        rtc.setTime(0, 0, 0, 1, 1, 2024);
+        }
+    } else {
+        rtc.setTime(0, 0, 0, 1, 1, 2024);
+    }
   #endif
-  #ifdef useSerialBT
-      SerialBT.printf("Connecting to %s ", WIFI_SSID);
-  #endif
-  WiFi.onEvent(WiFiStationHasIP, ARDUINO_EVENT_WIFI_STA_GOT_IP);
-  WiFi.onEvent(WiFiStationDisconnect, ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
-  WiFi.onEvent(WiFiScanComplete, ARDUINO_EVENT_WIFI_SCAN_DONE);
-  WiFi.onEvent(WiFiStaConnectedToSoftAP, ARDUINO_EVENT_WIFI_AP_STACONNECTED);
-  WiFi.disconnect(true);
-  WiFi.setHostname("AquariumController");
-  WiFi.mode(WIFI_STA);
-  WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, INADDR_NONE);
-  wifiEventSemaphore = xSemaphoreCreateBinary();
-  WiFi.setAutoReconnect(false);
-  WiFi.begin(ssid, password);
-  xSemaphoreTake(wifiEventSemaphore, portMAX_DELAY);
-  if (WiFi.status() == WL_CONNECTED) {
-      configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
-      if (getLocalTime(&timeinfo, 10000)) {
-      rtc.setTimeStruct(timeinfo);
-      } else {
-      rtc.setTime(0, 0, 0, 1, 1, 2024);
-      }
-  } else {
-      rtc.setTime(0, 0, 0, 1, 1, 2024);
-  }
   savedState.begin("aqController", false);
   aqWebServer.init();
   aqController.init(&aqWebServer);

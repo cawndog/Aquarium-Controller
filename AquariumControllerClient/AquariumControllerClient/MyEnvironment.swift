95dd3b4e74fa98d108b4357d2b736e876ac1cbe1
cawndog <c.jwilliams@me.com>
2024-10-28 08:57:22 -0600

    Create MyEnvironment.swift

---
+++ AquariumControllerClient/AquariumControllerClient/MyEnvironment.swift
@@ -0,0 +1,7 @@
+//Example MyEnvironment.swift file. 
+
+struct env {
+    static let bearerToken: String = "YbgRZ2i1jehAMGXXMXT4ek2TuG71k5ds7rlYRHGpbJbAfe7OqRoe806iID1dl0M4"
+    static let publicDNS: String = "myPublicDNS.com:8008"
+    static let privateIP: String = "192.168.0.2:8008"
+}
